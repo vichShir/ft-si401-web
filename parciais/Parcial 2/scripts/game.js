7aa95cb50e7c134c812281acae5be6b77f0eccaf
vichShir <victor.shirasuna@gmail.com>
2021-10-02 19:17:34 -0300

    Função do número de bombas exibido pelo tamanho do tabuleiro

--- parciais/Parcial 2/scripts/game.js
+++ parciais/Parcial 2/scripts/game.js
@@ -1,4 +1,30 @@
-function iniciarJogo()
+function mostrarBombas()
+{
+    var selectBombs = document.getElementById("select-bombas");
+    var conteudo_select = "";
+
+    var linhas = getTabelaDim()[0];
+    var colunas = getTabelaDim()[1];
+
+    selectBombs.innerHTML = "";
+    // Definir as opções
+    if (linhas <= 7 && colunas <= 7)
+    {
+        conteudo_select += "<option value='b3'>3</option>";
+        conteudo_select += "<option value='b4'>4</option>";
+        conteudo_select += "<option value='b5'>5</option>";
+    }
+    else
+    {
+        conteudo_select += "<option value='b10'>10</option>";
+        conteudo_select += "<option value='b12'>12</option>";
+        conteudo_select += "<option value='b15'>15</option>";
+    }
+
+    selectBombs.innerHTML = conteudo_select;
+}
+
+function getTabelaDim()
 {
     var myForm = document.forms["formulario-jogo"];
 
@@ -7,6 +33,16 @@ function iniciarJogo()
 
     var linhas = parseInt(tabsize.split("x")[0]);
     var colunas = parseInt(tabsize.split("x")[1]);
+
+    var dim = [linhas, colunas];
+
+    return dim;
+}
+
+function iniciarJogo()
+{
+    var linhas = getTabelaDim()[0];
+    var colunas = getTabelaDim()[1];
 
     montarTabela(linhas, colunas);
 }
