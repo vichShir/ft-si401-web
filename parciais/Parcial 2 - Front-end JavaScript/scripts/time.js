70759dd4bce13f4d9027373020f63a382fb1fe58
vichShir <victor.shirasuna@gmail.com>
2021-10-26 10:32:26 -0300

    Modificados stopwatch e timer

--- parciais/Parcial 2 - Front-end JavaScript/scripts/time.js
+++ parciais/Parcial 2 - Front-end JavaScript/scripts/time.js
@@ -1,59 +1,79 @@
 /**
- * Gerenciadores dos tempos
+ * Gerenciadores dos tempos (Stopwatch e Timer):
  */
 
 "use strict";
 
 /**
  * Classe responsável por controlar os detalhes do cronômetro:
- * - 
+ * - Interface com start, stop, reset do stopwatch e tempo final
+ * - Loop que controla o incremento dos tempos
+ * - Exibir o resultado no Front End
  * */
 class StopWatch
 {
-	static stopwatch = document.getElementById("stopwatch"); // Elemento html
+	static #stopwatchHTML; // Elemento html
 	static #cron; // Objeto setInterval
-  static #hour = 0;
-  static #minute = 0;
-  static #second = 0;
+
+  static #hour = 0; // Horas
+  static #minute = 0; // Minutos
+  static #second = 0; // Segundos
+  static #delay = 1000; // Delay em millisegundos
   
-	static start()
+  // Iniciar stopwatch
+	static start(stopwatchTag)
 	{
+    // Configurar variáveis
+    this.#stopwatchHTML = document.getElementById(stopwatchTag);
+
+    // Configurar ambiente
     this.reset();
-    this.#cron = setInterval(() => { this.#timer(); }, 1000);
+    this.#printHTML();
+
+    // Executar temporizador
+    this.#cron = setInterval(() => { this.#timeCycle(); }, this.#delay);
 	}
 
+  // Parar stopwatch de contabilizar
   static stop()
   {
     clearTimeout(this.#cron);
   }
 
+  // Zerar stopwatch
   static reset()
   {
     this.#hour = 0;
     this.#minute = 0;
     this.#second = 0;
-    this.#print();
-    clearTimeout(this.#cron);
+    this.stop();
   }
 
-  static #timer()
+  // Interfaces para pegar os tempos finais
+  static get finalHour() { return this.#hour; }
+  static get finalMinute() { return this.#minute; }
+  static get finalSecond() { return this.#second; }
+
+  // Controle do stopwatch
+  static #timeCycle()
   {
-    if ((this.#second += 1) == 60) 
+    if ((this.#second += 1) === 60) 
     {
       this.#second = 0;
-      this.#minute++;
+      this.#minute += 1;
     }
 
-    if (this.#minute == 60) 
+    if (this.#minute === 60) 
     {
       this.#minute = 0;
-      this.#hour++;
+      this.#hour += 1;
     }
 
-    this.#print();
+    this.#printHTML();
   }
 
-  static #print()
+  // Exibir no html
+  static #printHTML()
   {
     var second_text = this.#second.toLocaleString('en-US', {
       minimumIntegerDigits: 2,
@@ -67,54 +87,71 @@ class StopWatch
       minimumIntegerDigits: 2,
       useGrouping: false
     })
-		stopwatch.innerHTML = hour_text + ":" + minute_text + ":" + second_text;
+
+		this.#stopwatchHTML.innerHTML = hour_text + ":" + minute_text + ":" + second_text;
   }
 }
 
 /**
  * Classe responsável por controlar os detalhes do temporizador:
- * - 
+ * - Interface com executar função quando timer acabar
+ * - Interface com start, stop e reset do timer
+ * - Converter segundos em horas, minutos e segundos
+ * - Loop que controla o decremento dos tempos
+ * - Exibir o resultado no Front End
  * */
 class Timer
 {
 	static #timerHTML; // Elemento html
-	static #timer; // Objeto setTimeout
-	static #totalSeconds; // Tempo total
-	static #second = 0; // Segundos
-	static #minute = 0; // Minutos
+	static #timer; // Objeto setInterval
+
+	static #totalSeconds; // Tempo inicial do temporizador
 	static #hour = 0; // Horas
+	static #minute = 0; // Minutos
+  static #second = 0; // Segundos
+  static #delay = 1000; // Delay em millisegundos
 
-  static #callback; // Função a ser executada quando o timer acabar
+  static #callbackFunction; // Função a ser executada quando o timer acabar
 
-	static start(totalSeconds)
+  // Executar função quando o timer zerar
+  static execOnFinish(callback)
   {
-    this.#timerHTML = document.getElementById("timer");
-    this.#totalSeconds = totalSeconds;
-    this.reset();
-    this.#print();
-    this.#convertToTime();
-    this.#timer = setInterval(() => { this.#timeCycle(); }, 1000);
+    this.#callbackFunction = callback;
   }
 
+  // Iniciar timer
+	static start(timerTag, totalSeconds)
+  {
+    // Configurar variáveis
+    this.#timerHTML = document.getElementById(timerTag);
+    this.#totalSeconds = totalSeconds;
+
+    // Configurar ambiente
+    this.reset();
+    this.#convertSecondsToMyTime();
+    this.#printHTML();
+
+    // Executar temporizador
+    this.#timer = setInterval(() => { this.#timeCycle(); }, this.#delay);
+  }
+
+  // Parar timer de contabilizar
   static stop()
   {
     clearTimeout(this.#timer);
   }
 
+  // Zerar timer
   static reset()
   {
     this.#minute = 0;
     this.#second = 0;
     this.#hour = 0;
-    clearTimeout(this.#timer);
+    this.stop();
   }
 
-  static execOnFinish(callback)
-  {
-    this.#callback = callback;
-  }
-
-  static #convertToTime()
+  // Converter segundos em horas, minutos e segundos
+  static #convertSecondsToMyTime()
   {
     this.#hour = Math.floor(this.#totalSeconds / 3600);
     this.#totalSeconds %= 3600;
@@ -122,6 +159,7 @@ class Timer
     this.#second = this.#totalSeconds % 60;
   }
 
+  // Controle do timer
   static #timeCycle()
   {
     if(this.#second > 0)
@@ -146,14 +184,16 @@ class Timer
         else
         {
           this.stop();
-          this.#callback();
+          this.#callbackFunction();
         }
       }
     }
-    this.#print();
+
+    this.#printHTML();
   }
 
-  static #print()
+  // Exibir no html
+  static #printHTML()
   {
     var second_text = this.#second.toLocaleString('en-US', {
       minimumIntegerDigits: 2,
